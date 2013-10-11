aa58e9f1713f85d49cdfdf3e12ad0869bdfa6f75
Joel Dwyer <joel.dwyer@nodramas.com.au>
2013-10-12 10:18:47 +1100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bandrockr::Application.config.secret_key_base = 'da290f4386ed3afdae33cf425ac79da04021e4b2db3369bb0463c1a11d812ce53051b7ac562c31cbbdf76ed4a2b647491fd1012cd85ce74bf186c288a9a02b32'
